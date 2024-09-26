60d0a2abf4839449a2f9d22dc2b23d52764eec27
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2024-09-25 19:31:22 -0600

    Diseño de Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,57 @@
-<script setup>
-
-</script>
-
 <template>
-  <RouterView/>
+
+  <header>
+    <div class="navbar">
+      <NavBar />
+    </div>
+  </header>
+  <div class="container-router">
+    <RouterView />
+  </div>
+  
+
 </template>
 
+<script setup>
+import NavBar from './components/NavBar.vue'
+</script>
+
+
+
+
 <style scoped>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position:fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #53b2de;
+  margin-bottom: 2rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.navbar {
   display: flex;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
+  padding: 0.5rem 2rem;
+}
+
+
+.container-router {
+  display: flex;
   justify-content: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
+  align-items: center;
   
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
