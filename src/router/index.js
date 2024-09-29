3907c81c895dacefe9c11af0438979f33205bd22
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2024-09-29 11:36:07 -0600

    Eliminar archivo PruebaView.vue innecesario

--- src/router/index.js
+++ src/router/index.js
@@ -4,36 +4,31 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import SesionView from '../views/LoginViews/SesionView.vue'
 import MainLayout from '../components/LayoutComponents/MainLayout.vue'
-import PruebaView from '../views/LoginViews/PruebaView.vue'
 import MenuView from '../views/LoginViews/MenuView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      component: MainLayout,
-      children: [
-
-        {
-          path: '/prueba',
-          name: 'Prueba',
-          component: PruebaView
-        },
-        {
-          path: '/menu',
-          name: 'Menu',
-          component: MenuView
-        }
-      ],
-      
-    },
-    {
-      path: '/sesion',
-      name: 'sesion',
-      component: SesionView
-    },
-  ]
+    
+      {
+        path: '/',
+        name: 'sesion',
+        component: SesionView
+      },
+      {
+        path: '/Main',
+        component: MainLayout,
+        children: [
+          {
+            path: '/Menu',
+            name: 'Menu',
+            component: MenuView
+          }
+        ],
+        
+      },
+    
+      ]
 })
 
 export default router
