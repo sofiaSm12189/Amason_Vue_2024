ed574c902a6d408f83bfabeebcee6e98ae1597a1
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2024-10-21 10:07:29 -0600

    Añadir componente PopUpCart.vue y PopUpCartItem.vue para mostrar el carrito de compras con popup

---
+++ src/components/CartComponents/PopUpCart.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="minicart">
+    <div class="minicart--item-container">
+      ¡Tienes <span class="minicart--item-count" style="font-weight: 600">1 item(s)</span> en tu carrito!
+    </div>
+    <hr>
+    <popUpCartItem/>
+    <hr>
+    <div class="minicart--subtotal">
+      <p class="minicart--subtotal-title">Subtotal</p>
+      <p class="minicart--subtotal-amount">$90.00</p>
+    </div>
+    
+    <div>
+      <input type="button" value="Ver detalles del carrito">
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import popUpCartItem from './PopUpCartItem.vue';
+export default {
+  components: {
+    popUpCartItem,
+    
+  },
+
+    
+}
+
+
+</script>
+
+<style scoped>
+
+
+.minicart {
+  font-family: sans-serif;
+  width: 290px;
+  height: auto;
+  padding: 20px;
+  background: #fff;
+  border-radius: 6px;
+  box-shadow: 0px 30px 130px -8px rgba(0, 0, 0, 0.25);
+  color: black;
+  text-align: left;
+  max-height: 600px;
+}
+
+
+
+.minicart input {
+  margin-top: 20px;
+  width: 100%;
+  height: 43px;
+  font-weight: 600;
+  background: white;
+  border: 2px solid #4babe2;
+  outline: none;
+  cursor: pointer;
+  transition: background 0.3s, color 0.3s;
+}
+
+.minicart input:hover {
+  color: #fff;
+  background: #4babe2;
+}
+
+.minicart--item-container {
+  margin-bottom: 10px;
+  font-size: .8em;
+  
+ 
+}
+
+.minicart hr {
+  margin-bottom: 20px;
+  width: 100%;
+}
+
+.minicart--subtotal {
+  margin: 20px 0;
+}
+
+.minicart--subtotal-title {
+  float: left;
+}
+
+.minicart--subtotal-amount {
+  float: right;
+  font-weight: 600;
+  font-size: 1.2em;
+}
+
+@media (max-height: 660px) {
+  .minicart {
+    max-height: 200px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+}
+</style>
---
+++ src/components/CartComponents/PopUpCartItem.vue
@@ -0,0 +1,84 @@
+<template>
+    <ul>
+      <li class="minicart--item" v-for="(product, index) in products" :key="index">
+        <div class="placeholder"></div>
+        <h1 class="title">{{ product.name }}</h1>
+        <p class="material">Vendedor: {{ product.vendedor }}</p>
+        <p class="price">Cantidad: {{ product.quantity }}</p>
+        <p class="price">Precio: {{ product.price }}</p>
+        <p class="remove">
+          <a href="#" @click.prevent="removeProduct(index)">
+            <i class="fa fa-trash-o"></i> Eliminar del carrito
+          </a>
+        </p>
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [
+          { name: 'Producto 01', vendedor: 'siwi', material: 'Material 01', size: '10x10', price: 90.00, quantity: 1 },
+          { name: 'Producto 02', vendedor: 'siwi',  material: 'Material 02', size: '15x15', price: 120.00, quantity: 2 },
+          { name: 'Producto 03', vendedor: 'siwi', material: 'Material 03', size: '20x20', price: 150.00, quantity: 1 }
+        ],
+      };
+    },
+    methods: {
+      removeProduct(index) {
+        this.products.splice(index, 1);
+      },
+    },
+  };
+  </script>
+  
+<style scoped>
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+.minicart--item {
+  margin-bottom: 10px;
+  width: 100%;
+  float: left;
+}
+
+.minicart--item > * {
+  margin-bottom: 5px;
+}
+
+.placeholder {
+  float: left;
+  margin-right: 10px;
+  width: 100px;
+  height: 100px;
+  background: #D8D8D8;
+}
+
+.title {
+  font-size: 1.1em;
+  font-weight: 600;
+  margin-bottom: 0;
+  margin-top: 0;
+}
+
+.material, .price {
+  font-size: .8em;
+  margin-bottom: 0;
+  margin-top: 0;
+}
+
+.remove {
+  margin-bottom: 0;
+  margin-top: 0;
+  font-size: .6em;
+}
+
+
+
+
+</style>
