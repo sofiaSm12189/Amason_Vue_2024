70dbc7faa916d3cc290f8b977bc37eb52d375f80
kristel.cordoba <kristel.cordoba@ucr.ac.cr>
2024-10-20 22:37:08 -0600

    Consumo de la Api de Login

--- src/components/LoginComponents/SignInCard.vue
+++ src/components/LoginComponents/SignInCard.vue
@@ -2,11 +2,15 @@
   <div class="container" id="container">
     <slot></slot>
     <div class="form-container sign">
-      <form>
+      <form @submit.prevent="login">
         <h1>Iniciar Sesión</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button class="Enviar" type="button" @click="handleLogin">Ingresar</button>
+        <label for="email">Email</label>
+      <input type="email" v-model="email" required />
+      
+      <label for="password">Password</label>
+      <input type="password" v-model="password" required />
+
+      <button type="submit">Login</button>
         <p>¿No tienes una cuenta?</p>
         <button type="button" @click="$emit('toggle')">Registrarse</button>
       </form>
@@ -14,18 +18,54 @@
   </div>
 </template>
 
-<script setup>
-
+<!-- <script setup>
 import { useRouter } from 'vue-router';
+
 
 const router = useRouter();
 
 const handleLogin = () => {
   router.push('/Menu'); 
 };
+
+</script> -->
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('http://localhost:8000/api/login', {
+          email: this.email,
+          password: this.password
+        });
+        
+        // Guarda el token en el localStorage para utilizarlo en futuras peticiones
+        const token = response.data.token;
+        localStorage.setItem('token', token);
+
+        // Configura Axios para incluir el token en los headers de las siguientes peticiones
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+        // Redirige al usuario a la página de tickets
+        this.$router.push('/Menu');
+      } catch (error) {
+        console.error('Login failed:', error);
+      }
+    }
+  }
+};
+
 </script>
 
 <style scoped>
 
 </style>
-
