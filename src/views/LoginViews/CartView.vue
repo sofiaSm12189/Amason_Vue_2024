5abfb12e8dd6000da766b603de9dba4ab1f6f14f
kristel.cordoba <kristel.cordoba@ucr.ac.cr>
2024-10-23 13:07:36 -0600

    Resumen de los montos de los artículos a comprar

--- src/views/LoginViews/CartView.vue
+++ src/views/LoginViews/CartView.vue
@@ -1,54 +1,76 @@
 <template>
+  <div class="body">
   <div class="container">
     <CarritoItem
       :products="[
+        { image: '@/assets/xiaomi.jpg', name: 'Audifonos', description: 'XIAOMI-auriculares inalámbricos Mini con Bluetooth 5,3, cascos TWS con Control táctil, deportivos, resistentes al agua, para juegos', price: 15, delivery: 2, quantity: 1 },
         { image: '@/assets/xiaomi.jpg', name: 'Audifonos', description: 'XIAOMI-auriculares inalámbricos Mini con Bluetooth 5,3, cascos TWS con Control táctil, deportivos, resistentes al agua, para juegos', price: 15, delivery: 2, quantity: 1 },
         { image: '@/assets/xiaomi.jpg', name: 'Audifonos', description: 'XIAOMI-auriculares inalámbricos Mini con Bluetooth 5,3, cascos TWS con Control táctil, deportivos, resistentes al agua, para juegos', price: 15, delivery: 2, quantity: 1 },
       
       ]"
       @delete="handleDelete"
     />
+  </div> 
+   <div class="order"> <OrderCart/></div> 
   </div>
 </template>
 
 <script>
 import CarritoItem from '../../components/CartComponents/CartItem.vue';
+import OrderCart from '../../components/CartComponents/OrderCart.vue';
 
 export default {
   components: {
     CarritoItem,
+    OrderCart,
   },
   methods: {
-    //handleDelete(index) {
-      // Lógica para manejar la eliminación
-   // },
+    handleDelete(index) {
+      this.products.splice(index, 1); 
+    },
   },
 };
 </script>
 
 
 <style scoped>
-body{
+.body {
   background-color: #f3f3f3;
   font-family: 'Montserrat', sans-serif;
-  margin: 0;
-  padding: 0;
+  height: auto;
+  width: 100%;
+  padding: 10%;
+  padding: 10px;
   box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  align-items: space-around; 
 }
 
 .container {
   display: flex;
-  width: 1050px;
-  height: 500px;
-  margin-top: 24vh;
-  margin-left: 13vh;
-  padding: 13px 0px;
+  width: 100%;
+  max-width: 1020px;
+  height: auto;
+  margin-top: 20vh;
+  padding: 12px 0;
   flex-direction: column;
   align-items: flex-start;
-  gap: 2px;
-  flex-shrink: 0;
+  margin-bottom: 2%;
   border-radius: 12px;
-  background-color: #fff;
-  border: 2px solid #fff;
+  background-color: white;
+  box-sizing: border-box;
+  
 }
+
+.order {
+  margin-top: 8.6vh;
+  position: static;
+  height: auto;
+  gap: 40px;
+  padding: 20px;
+  border-radius: 12px;
+  box-sizing: border-box;
+}
+
 </style>
