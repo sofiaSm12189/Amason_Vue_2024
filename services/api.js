e3ab079c12708a4ccbcc799bb43107426643e429
kristel.cordoba <kristel.cordoba@ucr.ac.cr>
2024-10-25 03:00:35 -0600

    Cambios sin funcionalidad

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000/api', // Reemplaza esto con la URL base de tu API
+    baseURL: 'http://localhost:8000/api', 
     headers: {
         'Content-Type': 'application/json'
     }
