22173bfcdd2f0c919890d06fe74adb1c91de2056
Kristel Mariana Córdoba Carrillo <164565451+kristelC12@users.noreply.github.com>
2024-10-29 23:36:04 -0600

    Revert "Revert "Añadir al carrito""

--- src/components/MenuComponents/ListOfProducts&ProductDetails/ProductList.vue
+++ src/components/MenuComponents/ListOfProducts&ProductDetails/ProductList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="list-cotainer">
 
-    <h2><i class="fa-solid fa-laptop"></i> Tecnología <i class="fa-solid fa-headset"></i></h2>
+    <h2>{{ title }}</h2>
     <div class="product-list-cotainer">
-      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+      <ProductCard v-for="product in products" :key="product.product_id" :product="product" />
     </div>
 
   </div>
@@ -11,20 +11,49 @@
 
 <script>
 import ProductCard from "./ProductCard.vue";
-import { products } from "./mock-data";
+import axios from "axios";
 
 export default {
   components: { ProductCard },
   data() {
     return {
-      products,
+      products: [],
+      title: '',
     };
   },
+  async created() {
+    const categoryId = this.$route.query.categoryId;
+    const title = this.$route.query.title;
+    if (categoryId) {
+      this.title = title;
+      await this.fetchProductsByCategory(categoryId, title);
+    }
+  },
+  methods: {
+    async fetchProductsByCategory(categoryId) {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/products/category/${categoryId}`);
+        
+        // Reestructurar el JSON recibido
+        this.products = response.data.map(product => ({
+          product_id: product.product_id,
+          product_description: product.description,
+          product_name: product.name,
+          product_price: product.price,
+          product_stock: product.stock,
+          protuct_image: product.image
+        }));
+      } catch (error) {
+        console.error("Error al cargar productos por categoría:", error);
+      }
+    }
+  }
+
 };
 </script>
 
-<style scoped>
 
+<style scoped>
 .list-cotainer {
   display: flex;
   flex-direction: column;
@@ -41,7 +70,7 @@ export default {
   gap: 1rem;
 }
 
-@media (width <= 1030){
+@media (width <=1030) {
   .product-list-cotainer {
     width: 90vw;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
